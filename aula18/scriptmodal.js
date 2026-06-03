506982f46aec46381f3334e006e27d4d6e96a0a6
gabrielmorbach <gabrielmorbachsilva@gmail.com>
2026-06-03 15:13:11 -0300

    aula03/06/2026

---
+++ aula18/scriptmodal.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector(".modal");
+document.querySelector("#abrir")
+.addEventListener("click", () => {
+    modal.style.display = "flex";
+})
+document.querySelector("#fechar")
+.addEventListener("click", () => {
+    modal.style.display = "none";
+})
